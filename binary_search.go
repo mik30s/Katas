7adc2d3025e71b82c1bb36c49dc597b71930b826
Michael Osei <mosei27@live.com>
2017-06-12 16:19:23 -0700

    Add binary search

---
+++ binary_search.go
@@ -0,0 +1,28 @@
+package main
+import ("fmt")
+
+// rudimentary insertion sort
+func insertion_sort(list []int) []int {
+	for i := 1; i < len(list); i++ {
+		fmt.Printf("%v", i)
+		j := i - 1
+		for j > 0 {
+			if list[j] > list[j + 1] {
+				temp := list[j + 1]
+				list[j + 1] = list[j]
+				list[j] = temp
+				j--
+			}
+		}
+	}
+	return list
+}
+
+func binary_search(list []int) {
+	// first sort it.
+
+}
+
+func main() {
+	fmt.Printf("%v", insertion_sort([]int{1,3,4,2}))
+}
